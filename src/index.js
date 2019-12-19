33c3d66ee4ec4495db47050c205e8dcda202e8c5
Fatemeh Haji <fatemeh.haji@partdp.ir>
2019-12-19 15:14:13 +0330

    First Commit_Replace squares by numbers

---
+++ src/index.js
@@ -0,0 +1,68 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+
+
+class Square extends React.Component {
+    constructor(props){
+        super(props);
+            this.state={
+                value:null,
+            };
+    }
+    
+    render() {
+        return (
+            <button className="square"
+                onClick={() => this.setState({ value: 'X' })}>
+                {this.state.value}
+            </button>
+        );
+    }
+}
+class Board extends React.Component {
+
+    render() {
+        const status = "Next Player is : X";
+        return (
+            <div>
+                <div className="status">{status}</div>
+                <div className="board-row">
+                    <Square value={0} />
+                    <Square value={1} />
+                    <Square value={2} />
+                </div>
+                <div className="board-row">
+                    <Square value={3} />
+                    <Square value={4} />
+                    <Square value={5} />
+                </div>
+                <div className="board-row">
+                    <Square value={6} />
+                    <Square value={7} />
+                    <Square value={8} />
+                </div>
+            </div>
+        );
+    }
+}
+class Game extends React.Component {
+    render() {
+        return (
+            <div className="game">
+                <div className="game-board">
+                    <Board />
+                </div>
+                <div clas sName="game-info">
+                    <div>{/* status */}</div>
+                    <ol>{/* TODO */}</ol>
+                </div>
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(
+    <Game />,
+    document.getElementById('root')
+);
